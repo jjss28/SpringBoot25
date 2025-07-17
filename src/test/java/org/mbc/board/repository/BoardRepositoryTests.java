579b2ddc006daa131564b39266794d9f1c64b785
220-24\fuzzydice <brainjisu@gmail.com>
2025-07-17 16:39:46 +0900

    동적 쿼리문과 서비스 테스트 백업

--- src/test/java/org/mbc/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/mbc/board/repository/BoardRepositoryTests.java
@@ -147,12 +147,12 @@ public class BoardRepositoryTests {
     }
 
     @Test
-    public void testPaging() {
-        //.findAll()는 모든 리스트를 출력하는 메서드 select * from board;
-        // 전체 리스트에 페이징과 정렬 기법도 추가해보자.
+    public void testPaging(){
+        // .findAll() 는 모든 리스트를 출력하는 메서드 select * from board ;
+        // 전체 리스트에 페이징과 정렬 기법도 추가 해보자.
 
         Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
-        //                          시작 번호 10개 목록
+        //                           시작번호,페이지당 데이터 개수
         //                                       번호를 기준으로 내림차순 정렬!!!
         // Hibernate:
         //    select
@@ -165,36 +165,147 @@ public class BoardRepositoryTests {
         //    from
         //        board b1_0
         //    order by
-        //        b1_0.bno desc (bno를 기준으로 내림차순 정렬)
+        //        b1_0.bno desc  (bno를 기준으로 내림차순 정렬)
         //    limit
-        //        ?, ?  (시작번호, 끝번호)
-        // Hibernate: 
+        //        ?, ?         (시작번호, 끝번호)
+
+        //Hibernate:
         //    select
-        //        count(b1_0.bno) board 전체 리스트 수를 알아옴
+        //        count(b1_0.bno)   board 전체 리스트 수를 알아옴.
         //    from
         //        board b1_0
-
-
         Page<Board> result = boardRepository.findAll(pageable);
-        // 1장의 종이에 Board 객체를 가지고 있는 결과는 result에 담긴다.
-        // Page 클래스는 다음페이지 존재 여부, 이전페이지 존재 여부, 전체 데이터 갯수, 등등...계산을 한다.
+        // 1장에 종이에 Board 객체를 가지고 있는 결과는 result 에 담긴다.
+        // Page 클래스는 다음페이지 존재 여부, 이전페이지 존재 여부, 전체 데이터 개수, 등등.... 계산을 한다.
 
-        log.info("전체 게시물 수 : " + result.getTotalElements()); // 99
-        log.info("총 페이지 수 : " + result.getTotalPages()); // 10
-        log.info("현재 페이지 번호 : " + result.getNumber()); // 0
-        log.info("페이지당 데이터 갯수 : " + result.getSize()); // 10
-        log.info("다음페이지 여부 : " + result.hasNext()); // true
-        log.info("시작페이지 여부 : " + result.isFirst()); // true
-        
+        log.info("전체 게시물 수 : " + result.getTotalElements());  // 99
+        log.info("총 페이지 수 : " + result.getTotalPages());       // 10
+        log.info("현재 페이지 번호 : " + result.getNumber());       // 0
+        log.info("페이지당 데이터 개수 : " + result.getSize() );     // 10
+        log.info("다음페이지 여부 : " + result.hasNext());          // true
+        log.info("시작페이지 여부 : " + result.isFirst());         // true
+
         // 콘솔에 결과를 출력해보자.
         List<Board> boardList = result.getContent(); // 페이징처리된 내용을 가져와라
 
         boardList.forEach(board -> log.info(board));
-        // forEach는 인덱스를 사용하지 않고 앞에서부터 객체를 리턴함.
-        //              board -> log.info(board)
-        //                     람다식 1개의 명령어가 있을 때 활용
+        //   forEach는 인덱스를 사용하지 않고 앞에서부터 객체를 리턴함
+        //                board -> log.info(board)
+        //                      람다식 1개의 명령어가 있을 때 활용
 
     }
 
+    // 쿼리dsl 테스트 진행
+    @Test
+    public void testSearch1(){
 
+        Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.search1(pageable); //페이징 기법을 사용해서 title = 1 값을 찾아 오나?
+
+        result.getContent().forEach(board -> log.info(board));
+
+        //Hibernate:
+        //    select
+        //        b1_0.bno,
+        //        b1_0.content,
+        //        b1_0.moddate,
+        //        b1_0.regdate,
+        //        b1_0.title,
+        //        b1_0.writer
+        //    from
+        //        board b1_0
+        //    where
+        //        b1_0.title like ? escape '!'  -> like 1
+
+
+    // Hibernate:
+    //    select
+    //        b1_0.bno,
+    //        b1_0.content,
+    //        b1_0.moddate,
+    //        b1_0.regdate,
+    //        b1_0.title,
+    //        b1_0.writer
+    //    from
+    //        board b1_0
+    //    where
+    //        (
+    //            b1_0.title like ? escape '!'
+    //            or b1_0.content like ? escape '!'
+    //        )
+    //        and b1_0.bno>?
+    //    order by
+    //        b1_0.bno desc
+    //    limit
+    //        ?, ?
+    //Hibernate:
+    //    select
+    //        count(b1_0.bno)
+    //    from
+    //        board b1_0
+    //    where
+    //        (
+    //            b1_0.title like ? escape '!'
+    //            or b1_0.content like ? escape '!'
+    //        )
+    //        and b1_0.bno>?
+
+    }
+
+    @Test
+    public void testSearchAll() {
+        // 프론트에서 t가 선택되면 title, c가 선택되면 content, w가 선택되면 writer가 조건으로 게시됨
+
+        String[] types = {"t", "w"}; // 검색 조건
+
+        String keyword = "10"; // 검색 단어
+
+        Pageable pageable = PageRequest.of(0,10,Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+        // Hibernate:
+        //    select
+        //        b1_0.bno,
+        //        b1_0.content,
+        //        b1_0.moddate,
+        //        b1_0.regdate,
+        //        b1_0.title,
+        //        b1_0.writer
+        //    from
+        //        board b1_0
+        //    where
+        //        (
+        //            b1_0.title like ? escape '!'
+        //            or b1_0.content like ? escape '!'
+        //            or b1_0.writer like ? escape '!'
+        //        )
+        //        and b1_0.bno>?
+        //    order by
+        //        b1_0.bno desc
+        //    limit
+        //        ?, ?
+        //Hibernate:
+        //    select
+        //        count(b1_0.bno)
+        //    from
+        //        board b1_0
+        //    where
+        //        (
+        //            b1_0.title like ? escape '!'
+        //            or b1_0.content like ? escape '!'
+        //            or b1_0.writer like ? escape '!'
+        //        )
+        //        and b1_0.bno>?
+
+        log.info("전체 게시물 수 : " + result.getTotalElements());  // 99
+        log.info("총 페이지 수 : " + result.getTotalPages());       // 10
+        log.info("현재 페이지 번호 : " + result.getNumber());       // 0
+        log.info("페이지당 데이터 개수 : " + result.getSize() );     // 10
+        log.info("다음페이지 여부 : " + result.hasNext());          // true
+        log.info("시작페이지 여부 : " + result.isFirst());         // true
+
+        result.getContent().forEach(board -> log.info(board));
+
+    }
 } // 클래스 종료
